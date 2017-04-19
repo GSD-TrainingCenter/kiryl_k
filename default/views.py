535256dca645b838b7700835eec8fe937f9b5285
Kiryl Khaladzinski <earlmegget89@gmail.com>
2017-04-19 16:54:30 +0200

    On main page list of user interests

--- default/views.py
+++ default/views.py
@@ -6,7 +6,11 @@ from interests.models import Interest
 def home(request):
 
 	interests_list = Interest.objects.order_by('name')
+	user_interests_list = Interest.objects.filter(users=request.user).order_by('name')
 
-	context = {'interests': interests_list}
+	context = {
+		'interests': interests_list,
+		'user_interests': user_interests_list
+	}
 
 	return render(request, 'default/home.html', context)
